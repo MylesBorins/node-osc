5f0f1251cfeccf278e3a6b1c685441592de67eac
Myles Borins <myles@famo.us>
2015-05-18 01:40:15 -0700

    Add callback to client

--- test/test-naive.js
+++ test/test-naive.js
@@ -4,21 +4,39 @@ var test = require('tape');
 
 var osc = require('../lib');
 
-test('osc: message', function (t) {
+test('osc: argument message no callback', function (t) {
   var oscServer = new osc.Server(3333, '0.0.0.0');
   var client = new osc.Client('0.0.0.0', 3333);
 
+  t.plan(1);
+
   oscServer.on('message', function (msg) {
-    t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
     oscServer.kill();
     client.kill();
-    t.end();
+    t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
   });
 
   client.send('/test', 1, 2, 'testing');
 });
 
-test('osc: message', function (t) {
+test('osc: client with callback and message as arguments', function (t) {
+  var oscServer = new osc.Server(3333, '0.0.0.0');
+  var client = new osc.Client('0.0.0.0', 3333);
+
+  t.plan(2);
+
+  oscServer.on('message', function (msg) {
+    oscServer.kill();
+    t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
+  });
+
+  client.send('/test', 1, 2, 'testing', function (err) {
+    t.error(err, 'there should be no error');
+    client.kill();
+  });
+});
+
+test('osc: client with callback message as object', function (t) {
   var oscServer = new osc.Server(3333, '0.0.0.0');
   var client = new osc.Client('0.0.0.0', 3333);
   var m = new osc.Message('/address');
@@ -29,9 +47,10 @@ test('osc: message', function (t) {
   oscServer.on('message', function (msg) {
     t.deepEqual(msg, ['/address', 'testing', 'testing', 123], 'We should receive expected payload');
     oscServer.kill();
-    client.kill();
     t.end();
   });
 
-  client.send(m);
+  client.send(m, function () {
+    client.kill();
+  });
 });
