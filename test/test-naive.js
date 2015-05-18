d52890d870fed8915492ceecf24b5f3a5a956992
Myles Borins <myles@famo.us>
2015-05-18 00:08:58 -0700

    Some light linting

--- test/test-naive.js
+++ test/test-naive.js
@@ -7,7 +7,7 @@ var osc = require('../lib');
 test('osc: message', function (t) {
   var oscServer = new osc.Server(3333, '0.0.0.0');
   var client = new osc.Client('0.0.0.0', 3333);
-  
+
   oscServer.on('message', function (msg) {
     console.log('Message: ' + msg);
     t.ok(msg, 'this should exist');
@@ -15,6 +15,6 @@ test('osc: message', function (t) {
     client.kill();
     t.end();
   });
-  
+
   client.send('/test', 1, 2.3, 'testing');
 });
