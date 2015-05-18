1120551a80bf99f6b72e92d0e40f409a02c8c60d
Myles Borins <myles@famo.us>
2015-05-18 00:39:12 -0700

    Add another test

--- test/test-naive.js
+++ test/test-naive.js
@@ -17,3 +17,21 @@ test('osc: message', function (t) {
 
   client.send('/test', 1, 2, 'testing');
 });
+
+test('osc: message', function (t) {
+  var oscServer = new osc.Server(3333, '0.0.0.0');
+  var client = new osc.Client('0.0.0.0', 3333);
+  var m = new osc.Message('/address');
+  m.append('testing');
+  m.append('testing');
+  m.append(123);
+
+  oscServer.on('message', function (msg) {
+    t.deepEqual(msg, ['/address', 'testing', 'testing', 123], 'We should receive expected payload');
+    oscServer.kill();
+    client.kill();
+    t.end();
+  });
+
+  client.send(m);
+});
