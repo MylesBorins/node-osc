6aba5e6a540c95c6a0bc9b191875d74de7119ea1
Myles Borins <myles@famo.us>
2015-05-17 20:54:10 -0700

    Basic cleanup and tooling

--- examples/server.js
+++ examples/server.js
@@ -1,8 +1,9 @@
+'use strict';
+
 var osc = require('../lib/osc.js');
-var util = require('util');
 
 var oscServer = new osc.Server(3333, '0.0.0.0');
-oscServer.on("message", function (msg, rinfo) {
-    console.log("Message:");
+oscServer.on('message', function (msg) {
+    console.log('Message:');
     console.log(msg);
 });
---
+++ test/test-osc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var test = require('tape');
+
+var osc = require('../lib');
+
+test('osc: message', function (t) {
+  t.ok(osc, 'it exists');
+  t.end();
+});
