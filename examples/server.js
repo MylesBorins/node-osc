05e86b886ac3d95ea75f37b7c8cf2f8939467c37
Myles Borins <myles.borins@gmail.com>
2019-04-02 23:35:56 -0400

    test: Server at 100%

--- examples/server.js
+++ examples/server.js
@@ -7,4 +7,5 @@ var oscServer = new osc.Server(3333, '0.0.0.0');
 oscServer.on('message', function (msg) {
     console.log('Message:');
     console.log(msg);
+    oscServer.kill();
 });
--- test/test-server.js
+++ test/test-server.js
@@ -5,19 +5,28 @@ const test = require('tap').test;
 const osc = require('../lib');
 
 test('server: create and close', (t) => {
-  t.plan(2);
+  t.plan(1);
   const oscServer = new osc.Server(3333, '0.0.0.0');
   oscServer.close((err) => {
     t.error(err);
   });
-  t.ok(true);
+});
+
+test('server: bad message', (t) => {
+  t.plan(2);
+  const oscServer = new osc.Server(3333, '0.0.0.0');
+  t.throws(() => {
+    oscServer._sock.emit('message', 'whoops');
+  }, /can't decode incoming message:/);
+  oscServer.close((err) => {
+    t.error(err);
+  });
 });
 
 test('server: legacy kill alias', (t) => {
-  t.plan(2);
+  t.plan(1);
   const oscServer = new osc.Server(3333, '0.0.0.0');
-  oscServer.close((err) => {
+  oscServer.kill((err) => {
     t.error(err);
   });
-  t.ok(true);
 });
