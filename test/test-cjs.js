7484077e0ac8a4483c2ecf0c7d85508f40179c0f
Myles Borins <myles.borins@gmail.com>
2019-12-07 18:47:58 -0500

    build: auto generate CJS

--- test/test-cjs.js
+++ test/test-cjs.js
@@ -2,47 +2,40 @@
 
 const test = require('tap').test;
 
-// const generatePort = require('./generate-port');
+const generatePort = require('./generate-port');
 
-test('cjs: noop', (t) => {
-  t.done();
+const osc = require('node-osc');
+
+test('osc: argument message no callback', (t) => {
+  const port = generatePort();
+  const oscServer = new osc.Server(port, '0.0.0.0');
+  const client = new osc.Client('0.0.0.0', port);
+
+  t.plan(1);
+
+  oscServer.on('message', (msg) => {
+    oscServer.close();
+    client.close();
+    t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
+  });
+
+  client.send('/test', 1, 2, 'testing');
 });
 
+test('osc: client with callback and message as arguments', (t) => {
+  const port = generatePort();
+  const oscServer = new osc.Server(port, '0.0.0.0');
+  const client = new osc.Client('0.0.0.0', port);
 
-// const osc = require('node-osc');
-//
-// test('osc: argument message no callback', (t) => {
-//   const port = generatePort();
-//   const oscServer = new osc.Server(port, '0.0.0.0');
-//   const client = new osc.Client('0.0.0.0', port);
-//
-//   t.plan(1);
-//
-//   oscServer.on('message', (msg) => {
-//     oscServer.close();
-//     client.close();
-//     t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
-//   });
-//
-//   client.send('/test', 1, 2, 'testing');
-// });
-//
-// test('osc: client with callback and message as arguments', (t) => {
-//   const port = generatePort();
-//   const oscServer = new osc.Server(port, '0.0.0.0');
-//   const client = new osc.Client('0.0.0.0', port);
-//
-//   t.plan(2);
-//
-//   oscServer.on('message', (msg) => {
-//     oscServer.close();
-//     t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
-//   });
-//
-//   client.send('/test', 1, 2, 'testing', (err) => {
-//     t.error(err, 'there should be no error');
-//     client.close();
-//   });
-// });
-//
-//
+  t.plan(2);
+
+  oscServer.on('message', (msg) => {
+    oscServer.close();
+    t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
+  });
+
+  client.send('/test', 1, 2, 'testing', (err) => {
+    t.error(err, 'there should be no error');
+    client.close();
+  });
+});
