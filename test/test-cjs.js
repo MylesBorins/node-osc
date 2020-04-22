ca15406c92a3455e629d6c0c952acf9ffd386692
Myles Borins <myles.borins@gmail.com>
2020-04-22 02:27:27 -0400

    test: tiny refactors

--- test/test-cjs.js
+++ test/test-cjs.js
@@ -9,7 +9,7 @@ const osc = require('../');
 
 t.beforeEach(async (done, t) => {
   t.context.port = await getPort({
-    port: getPort.makeRange(3000, 3100)
+    port: getPort.makeRange(3000, 3500)
   });
 });
 
@@ -17,12 +17,9 @@ test('osc: argument message no callback', (t) => {
   const oscServer = new osc.Server(t.context.port, '127.0.0.1');
   const client = new osc.Client('127.0.0.1', t.context.port);
 
-  t.plan(1);
-
   oscServer.on('message', (msg) => {
-    oscServer.close();
-    client.close();
     t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
+    oscServer.close(client.close(t.done));
   });
 
   client.send('/test', 1, 2, 'testing');
@@ -32,15 +29,12 @@ test('osc: client with callback and message as arguments', (t) => {
   const oscServer = new osc.Server(t.context.port, '127.0.0.1');
   const client = new osc.Client('127.0.0.1', t.context.port);
 
-  t.plan(2);
-
   oscServer.on('message', (msg) => {
-    oscServer.close();
     t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
+    client.close(oscServer.close(t.done));
   });
 
   client.send('/test', 1, 2, 'testing', (err) => {
     t.error(err, 'there should be no error');
-    client.close();
   });
 });
