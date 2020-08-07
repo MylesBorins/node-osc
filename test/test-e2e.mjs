5fb398b8ddd5b8900ea85b9763426210570e994f
Myles Borins <myles.borins@gmail.com>
2020-08-07 02:47:52 -0400

    mebeh?

--- test/test-e2e.mjs
+++ test/test-e2e.mjs
@@ -4,23 +4,47 @@ import { Server, Client } from 'node-osc';
 
 tap.beforeEach(beforeEach);
 
-if (process.release.lts === 'Dubium' &&
-    process.platform === 'win32') {
-  tap.skip('Flaky on 10.x on windows');
-} else {
-  test('osc: argument message no callback', (t) => {
-    const oscServer = new Server(t.context.port, '0.0.0.0');
-    const client = new Client('0.0.0.0', t.context.port);
-
-    t.plan(1);
-
-    oscServer.on('message', (msg) => {
-      oscServer.close();
-      client.close();
-      t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
-    });
-
-    client.send('/test', 1, 2, 'testing');
-  });
-  
+function flaky() {
+  return process.release.lts === 'Dubnium' && process.platform === 'win32';
 }
+
+function skip(t) {
+  t.skip(`flaky ~ ${t.name}`);
+  t.done();
+}
+
+test('osc: argument message no callback', (t) => {
+  if (flaky()) return skip(t);
+
+  const oscServer = new Server(t.context.port, '0.0.0.0');
+  const client = new Client('0.0.0.0', t.context.port);
+
+  t.plan(1);
+
+  oscServer.on('message', (msg) => {
+    oscServer.close();
+    client.close();
+    t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
+  });
+
+  client.send('/test', 1, 2, 'testing');
+});
+
+test('osc: client with callback and message as arguments', (t) => {
+  if (flaky()) return skip(t);
+
+  const oscServer = new Server(t.context.port, '0.0.0.0');
+  const client = new Client('0.0.0.0', t.context.port);
+
+  t.plan(2);
+
+  oscServer.on('message', (msg) => {
+    oscServer.close();
+    t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
+  });
+
+  client.send('/test', 1, 2, 'testing', (err) => {
+    t.error(err, 'there should be no error');
+    client.close();
+  });
+});
