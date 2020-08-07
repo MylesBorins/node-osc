fa35e8406cb4b3dcc33c5adbcd657332979f44d7
Myles Borins <myles.borins@gmail.com>
2020-08-07 02:13:41 -0400

    poof

--- test/test-e2e.mjs
+++ test/test-e2e.mjs
@@ -18,22 +18,3 @@ test('osc: argument message no callback', (t) => {
 
   client.send('/test', 1, 2, 'testing');
 });
-
-test('osc: client with callback and message as arguments', (t) => {
-  const oscServer = new Server(t.context.port, '0.0.0.0');
-  const client = new Client('0.0.0.0', t.context.port);
-
-  t.plan(2);
-
-  oscServer.on('message', (msg) => {
-    oscServer.close();
-    t.deepEqual(msg, ['/test', 1, 2, 'testing'], 'We should receive expected payload');
-  });
-
-  client.send('/test', 1, 2, 'testing', (err) => {
-    t.error(err, 'there should be no error');
-    client.close();
-  });
-});
-
-
