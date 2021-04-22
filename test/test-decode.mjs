ae1a636e90259fc2aa77f6127f47be429ff2d2f7
Myles Borins <myles.borins@gmail.com>
2021-04-22 00:28:21 -0400

    deps: remove jspack

--- test/test-decode.mjs
+++ test/test-decode.mjs
@@ -2,9 +2,15 @@ import { test } from 'tap';
 
 import decode from 'node-osc/decode';
 
-test('decode: empty', (t) => {
+test('decode: no message', (t) => {
   const buf = Buffer.from('/test\0');
-  t.same(decode(buf), [], 'should be empty array');
+  t.same(decode(buf), ['/test'], 'should have just an address');
+  t.end();
+});
+
+test('decode: valid', (t) => {
+  const buf = Buffer.from('/test\0\0\0,s\0,testing\0');
+  t.same(decode(buf), ['/test', 'testing'], 'should be empty array');
   t.end();
 });
 
@@ -16,30 +22,8 @@ test('decode: valid', (t) => {
 
 test('decode: invalid typetags', (t) => {
   t.throws(() => {
-    const buf = Buffer.from('/test\0iii\0');
-    decode(buf);
-  }, /invalid type tag in incoming OSC message, must start with comma/);
-  t.throws(() => {
     const buf = Buffer.from('/test\0\0\0,R\0');
     decode(buf);
-  }, /Unsupported OSC type tag/);
-  t.end();
-});
-
-test('decode: valid', (t) => {
-  const buf = Buffer.from('/test\0\0\0,s\0,testing\0');
-  t.same(decode(buf), ['/test', 'testing'], 'should be empty array');
-  t.end();
-});
-
-test('decode: invalid typetags', (t) => {
-  t.throws(() => {
-    const buf = Buffer.from('/test\0iii\0');
-    decode(buf);
-  }, /invalid type tag in incoming OSC message, must start with comma/);
-  t.throws(() => {
-    const buf = Buffer.from('/test\0\0\0,R\0');
-    decode(buf);
-  }, /Unsupported OSC type tag/);
+  }, /I don't understand the argument code R/);
   t.end();
 });
