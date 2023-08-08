7f7d0e6b31c246b3581eaa4d8bc90d65d907183e
Myles Borins <mylesborins@github.com>
2023-08-08 13:12:19 -0400

    server: ensure host argument is used
    Fixes: https://github.com/MylesBorins/node-osc/issues/98

--- test/test-server.mjs
+++ test/test-server.mjs
@@ -13,11 +13,60 @@ test('server: create and close', (t) => {
   });
 });
 
-test('client: listen to message', (t) => {
-  const oscServer = new Server(t.context.port, '127.0.0.1');
+test('server: listen to message', (t) => {
+  const oscServer = new Server(t.context.port);
   const client = new Client('127.0.0.1', t.context.port);
 
   t.plan(3);
+
+  t.teardown(() => {
+    oscServer.close();
+    client.close();
+  });
+
+  oscServer.on('message', (msg) => {
+    t.same(msg, ['/test'], 'We should receive expected payload');
+  });
+  
+  oscServer.on('/test', (msg) => {
+    t.same(msg, ['/test'], 'We should receive expected payload');
+  });
+
+  client.send('/test', (err) => {
+    t.error(err, 'there should be no error');
+  });
+});
+
+test('server: no defined host', (t) => {
+  const oscServer = new Server(t.context.port);
+  const client = new Client('127.0.0.1', t.context.port);
+
+  t.plan(3);
+
+  t.teardown(() => {
+    oscServer.close();
+    client.close();
+  });
+
+  oscServer.on('message', (msg) => {
+    t.same(msg, ['/test'], 'We should receive expected payload');
+  });
+  
+  oscServer.on('/test', (msg) => {
+    t.same(msg, ['/test'], 'We should receive expected payload');
+  });
+
+  client.send('/test', (err) => {
+    t.error(err, 'there should be no error');
+  });
+});
+
+test('server: callback as second arg', (t) => {
+  t.plan(4);
+  const oscServer = new Server(t.context.port, () => {
+    t.ok('callback called');
+  });
+  const client = new Client('127.0.0.1', t.context.port);
 
   t.teardown(() => {
     oscServer.close();
